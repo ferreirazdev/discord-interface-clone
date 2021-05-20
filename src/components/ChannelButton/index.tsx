e9a605752238f04142e71372e0ee2c13fd6f246a
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-20 11:54:15 -0300

    Channel Button

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles'
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Title>Servidor do Irineu</Title>
+
+      <ExpandIcon />
+    </Container>
+  )
+};
+
+export default Layout;
