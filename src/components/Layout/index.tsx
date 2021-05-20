ffbe03fcd7e58b000bb90f24f13946bd3cdb6941
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-20 18:12:31 -0300

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,6 +6,9 @@ import ServerList from '../ServerList';
 import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
+import UserInfo from '../UserInfo';
+import UserList from '../UserList';
+import ChannelData from '../ChannelData';
 
 const Layout: React.FC = () => {
   return (
@@ -14,6 +17,9 @@ const Layout: React.FC = () => {
       <ServerName />
       <ChannelInfo />
       <ChannelList />
+      <UserInfo />
+      <ChannelData />
+      <UserList />
     </Grid>
   );
 };
